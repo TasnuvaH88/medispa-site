e6e17207781becb15aae56c71edbc264e59f9c23
Tasnuva <tasnuvah88@gmail.com>
2018-09-06 22:33:53 -0400

    fixed the containers

--- js/init.js
+++ js/init.js
@@ -1,8 +1,9 @@
 (function($){
   $(function(){
-
+    
     $('.sidenav').sidenav();
     $('.parallax').parallax();
-
+    $(".dropdown-trigger").dropdown();
+    $('.slider').slider();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
