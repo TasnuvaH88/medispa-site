35cbbd07e7b036a909588d11341649865671545b
Tasnuva <tasnuvah88@gmail.com>
2018-11-02 18:17:35 -0400

    updated banner

--- js/init.js
+++ js/init.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   $('.slider').slider();
-    $('.parallax').parallax();
-    
-    $(".dropdown-trigger").dropdown();
+  $('.parallax').parallax();
+  
+ $(".dropdown-trigger").dropdown();
     
     $('.sidenav').sidenav();
   }); // end of document reaay
