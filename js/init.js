2274a1f20fdf6acc238f21eb55bf74c18090ad41
Tasnuva <tasnuvah88@gmail.com>
2018-11-02 18:02:41 -0400

    fixed loading order issues

--- js/init.js
+++ js/init.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
+  $('.slider').slider();
     $('.parallax').parallax();
-    $('.sidenav').sidenav();
+    
     $(".dropdown-trigger").dropdown();
-    $('.slider').slider();
+    
+    $('.sidenav').sidenav();
   }); // end of document reaay
